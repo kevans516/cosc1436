ef3d4bc54de76511cbe92f05fb4e91cd7067ecb2
Kevin Evans <kevin.evans516@my.tccd.edu>
2025-09-04 21:28:30 -0500

    discussed variable and types and assignment

--- classwork/Chapter2/Chapter2/Chapter2.cpp
+++ classwork/Chapter2/Chapter2/Chapter2.cpp
@@ -9,8 +9,6 @@
 int main()
 {
     
-    std::cout << "Hello World" << "Your Name" << std::endl;
-    std::cout << "You are" << " " << 20 << " " << "Years old" << std::endl;
     std::string name;
     name = "Name";
     std::cout << "Hello World" << name;
