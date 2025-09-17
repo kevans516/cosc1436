37d6cd8c713c10ec3a616326080c67af9bc83ae0
Kevin Evans <kevin.evans516@my.tccd.edu>
2025-09-16 21:24:20 -0500

    Completed chapter 3

--- classwork/Chapter 3/Chapter 3/Chapter 3.cpp
+++ classwork/Chapter 3/Chapter 3/Chapter 3.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
 #include <string>
+#include <cmath>
+#include <iomanip>
 
 void main()
+{
+    std::cout << "Enter values for X and Y: ";
+
+    double x, y;
+    std::cin >> x >> y;
+
+    // Function X  Y  Result
+    //----------------------
+    std::cout << std::fixed;
+    std::cout << std::setprecision(4);
+
+    std::cout << std::setw(9) << "Function " << std::setw(7) << " X " << std::setw(7) << " Y " << std::setw(12) << "Result" << std::endl;
+
+    std::cout << std::setw(35) << std::setfill('-') << std::setfill(' ') << std::endl;
+
+    std::cout << std::setw(9) << "pow " << std::setw(9) << x << std::setw(9) << y << std::setw(9) << pow(x, y) << std::endl;
+
+    std::cout << std::setw(9) << "sqrt " << std::setw(9) << std::setw(9) << x << std::setw(9) << y << sqrt(x) << std::endl;
+
+    std::cout << std::setw(9) << "ceil " << std::setw(9) << x << std::setw(9) << y << ceil(x) << std::endl;
+
+    std::cout << std::setw(9) << "floor " << std::setw(9) << x << std::setw(9) << y << floor(x) << std::endl;
+
+    std::cout << std::setw(9) << "round " << x << y << round(x) << std::endl;
+
+    std::cout << "trunc " << x << y << trunc(x) << std::endl;
+
+}
+
+void ExpressionDemo()
 {
     //Prompt user for radius of circle
     std::cout << "Enter radius: ";
@@ -70,5 +102,46 @@ void main()
     intDivision = static_cast<double>(left) / right;  // double / int => double
     intDivision = left / static_cast<double>(right); // int / double => double
 
-    intDivision = (int)"Hello"; 
+    //This is wrong and will not produce 5
+    intDivision = (int)"5";
+    //intDivision = static_cast<int>("5"); //Compiler error
+
+    //Math functions 
+    // pow(x, y) => power X to the Y power
+    // sqrt(x) => square root of x
+    // abs(x) => absolute value of x
+    //
+    //ceil(x) => smallest possible integral value >= X
+    // floor(x) => Largetst possible integral value >= X
+    // 
+    // round(x) => Rounds a float to an int using midpoint rounding
+    // trunc(x) => Truncates a float to an int
+    //
+    // exp(x) => exponential, E raises to X power, E = 2.17828
+    // log(x) => logarithm, inverse of the exp
+
+    double result;
+    result = pow(5, 3); //5 * 5 * 5 = 125
+    result = pow(125.0, -3.0); //cube root of 125
+
+    result = sqrt(100); // 10
+    result = sqrt(pow(5, 2)); // 5
+
+    result = abs(10); // 10
+    result = abs(-10); //10
+
+    result = ceil(14.5); // 15
+    result = floor(3.1); // 3
+
+    result = round(14.5); //15
+    result = round(3.1); //3
+
+    result = trunc(14.5); // 14
+    result = trunc(3.1); // 3
+    result = (int)14.5;
+
+    result = exp(4);
+    result = log(exp(4)); // 4
+
+
 }
